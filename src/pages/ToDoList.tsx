83f1eae858a0034ebc3454ac9f7010964eb747ef
Cwoodard2 <cawooda@siue.edu>
2024-09-05 13:09:43 -0500

    initial commit

---
+++ src/pages/ToDoList.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'preact/hooks';
+
+function ToDoList() {
+    interface todoList {
+        task: string,
+        deadline: string,
+        subTasks: string[]
+    };
+
+    const [todos, setTodos] = useState<todoList[]>([{task: '', deadline: '', subTasks: []}]);
+    const [newTask, setNewTask] = useState<string>('');
+    const [newDeadline, setDeadline] = useState<string>('');
+
+    function changeTask(e: any) {
+        setNewTask(e.target.value);
+    }
+
+    function changeDeadline(e: any) {
+        setDeadline(e.target.value);
+    }
+
+    function addTask() {
+        setTodos(
+            todos.concat({task: newTask, deadline: newDeadline, subTasks: []})
+        );
+        setNewTask('');
+    }
+
+    function finishTask(task: any) {
+        console.log(task);
+        const newList = todos.filter((todo: any) => todo !== task);
+        console.log(newList);
+        setTodos(newList);
+    }
+
+    return (
+        <>
+<div>
+    <input type={'text'} name="task" value={newTask} onChange={(e: any) => changeTask(e)}></input>
+    <input type={'text'} name="deadline" value={newDeadline} onChange={(e: any) => changeDeadline(e)}></input>
+    <button onClick={() => addTask()}>Add</button>
+</div>
+<div>
+    <ul>
+    {todos.map((todo: any, key: number) => <li key={key}>{todo.task}{todo.deadline}<button onClick={() => finishTask(todo)}>Complete</button></li>)}
+    </ul>
+</div>
+        </>
+    )
+}
+
+export default ToDoList;
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'adhdApp',
+      short_name: 'adhdApp',
+      description: 'This app contains feature to help someone with ADHD maintain a healthy lifestyle.',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})

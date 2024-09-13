f2bb530fe71e2e6d0e24ecef43881df1ccaa65f8
Cwoodard2 <cawooda@siue.edu>
2024-09-13 01:17:04 -0500

    - Forgot to add to navigation commit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,33 @@
+import home from "../assets/navigation-images/Home-Icon.svg";
+import list from "../assets/navigation-images/List-Alt-Icon.svg";
+
 export default function Navigation() {
   return (
     <>
-      <div class="sticky w-full bottom-0 left-0 md:top-0 md:left-0 md:w-20 md:m-0">
-        <nav class="flex flex-row md:flex-col border-t-2 md:border-t-0 md:border-r-2 border-black md:min-h-screen p-4 justify-evenly md:justify-normal">
-          <a href="/dashboard">Home</a>
-          <a href="/todo-list">Todo</a>
+      <div class="sticky bottom-0 left-0 md:top-0 md:left-0 md:w-80 md:m-0 bg-gray-200 md:rounded-r-lg">
+        <p>{document.title}</p>
+        <nav class="flex flex-row md:flex-col border-t-2 md:border-t-0 md:min-h-screen p-4 justify-evenly md:justify-normal">
+          <a
+            href="/dashboard"
+            class="flex flex-row gap-3 hover:bg-gray-300 p-1 rounded-md"
+          >
+            <img src={home} alt="home icon" class="text-black" />
+            Home
+          </a>
+          <a
+            href="/todo-list"
+            class="flex flex-row gap-3 hover:bg-gray-300 p-1 rounded-md"
+          >
+            <img src={list} alt="home icon" />
+            Todo
+          </a>
+          <a
+            href="/weekly-planner"
+            class="flex flex-row gap-3 hover:bg-gray-300 p-1 rounded-md"
+          >
+            <img src={list} alt="home icon" />
+            Weekly Planner
+          </a>
         </nav>
       </div>
     </>
